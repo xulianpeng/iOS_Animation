bdcff18f7ba676ec384e6519f57dd1e9a4e0821b
xulianpeng <xulianpengxlp@126.com>
2017-02-08 18:30:01 +0800

    UIView动画 Demo
    UIView动画 Demo

--- iOS_Animation/ViewController.m
+++ iOS_Animation/ViewController.m
@@ -7,7 +7,8 @@
 //
 
 #import "ViewController.h"
-#import "Masonary.h"
+#import "Masonry.h"
+
 @interface ViewController ()
 {
     UIView *parentView;
@@ -43,7 +44,28 @@
     
     
     UIButton *bt = [UIButton buttonWithType:UIButtonTypeCustom];
-    bt.
+    bt.backgroundColor = [UIColor blueColor];
+    [bt addTarget:self action:@selector(beginAnimationClick) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:bt];
+    [bt mas_makeConstraints:^(MASConstraintMaker *make) {
+        
+        make.left.equalTo(self.view).offset(30);
+        make.top.mas_equalTo(100);
+        make.width.mas_equalTo(50);
+        make.height.mas_equalTo(40);
+    }];
+    
+    UIButton *resetBt = [UIButton buttonWithType:UIButtonTypeCustom];
+    resetBt.backgroundColor = [UIColor redColor];
+    [resetBt addTarget:self action:@selector(resetFrame:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:resetBt];
+    [resetBt mas_makeConstraints:^(MASConstraintMaker *make) {
+        
+        make.left.equalTo(bt.mas_right).offset(30);
+        make.top.mas_equalTo(bt);
+        make.width.mas_equalTo(50);
+        make.height.mas_equalTo(40);
+    }];
 #pragma mark 基本用法1: 一般舍弃
      
     /*
@@ -68,169 +90,6 @@
     //小结:显然这种动画方式 写法很费事 代码多 且无法实现放大的效果 因为其代码执行是顺序执行.目前只实现位移的效果 只改变x y 或同时改变xy.
     */
     
-    
-#pragma mark 基本用法2: UIViewAnimation 最简单的Block动画
-    
-    [UIView animateWithDuration:2 animations:^{
-        testLabel1.frame = CGRectMake(300 - 80, 200 - 60, 80, 60) ;
-    }];
-
-//    [UIView animateWithDuration:2 animations:^{
-//        testLabel1.frame = CGRectMake(300 - 80, 200 - 60, 80, 60) ;
-//    } completion:^(BOOL finished) {
-//        
-//        
-//    }];
-    
-    
-    
-    //UIViewAnimationOptionRepeat //单一则只是重复动画的过程
-    //UIViewAnimationOptionAutoreverse //单一 重复动画的反过程
-    //以上两者合并 则实现动画重复
-    [UIView animateWithDuration:2 delay:0 options:UIViewAnimationOptionRepeat|UIViewAnimationOptionAutoreverse|UIViewAnimationOptionCurveEaseIn animations:^{
-        testLabel1.frame = CGRectMake(300 - 80, 200 - 60, 80, 60) ;
-    } completion:^(BOOL finished) {
-        
-    }];
-    
-    
-    /*
-    //动画 放大 后消失
-    [UIView animateWithDuration:2 animations:^{
-        testLabel1.transform = CGAffineTransformMakeScale(1.5f, 1.5f);
-        testLabel1.alpha = 0.f;
-    } completion:^(BOOL finished) {
-        [testLabel1 removeFromSuperview];
-    }];
-     */
-    //弹性动画
-    //弹性效果的实现:必须有
-    /*
-    [UIView animateWithDuration:2 delay:0 usingSpringWithDamping:0.1 initialSpringVelocity:15 options:UIViewAnimationOptionCurveEaseInOut animations:^{
-        testLabel1.center = CGPointMake(150 - 5, testLabel1.center.y);
-//         testLabel1.frame = CGRectMake(0, 0, 80, 60) ;
-    } completion:^(BOOL finished) {
-        
-        [UIView animateWithDuration:2 animations:^{
-            testLabel1.transform = CGAffineTransformMakeScale(1.5f, 1.5f);
-            testLabel1.alpha = 0.f;
-        } completion:^(BOOL finished) {
-            [testLabel1 removeFromSuperview];
-        }];
-    }];
-    //小结:动画可以串联
-    */
-//作业: 如何实现输入框在输入错误的情况下 抖动
-    
-    
-    /*
-    [UIView animateKeyframesWithDuration:15
-                                   delay:3
-                                 options:(UIViewKeyframeAnimationOptionCalculationModeLinear)
-                              animations:^{ // 参数1: RelativeStartTime: 相对的开始时间 参数2: relativeDuration:相对持续时间
-                                  [UIView addKeyframeWithRelativeStartTime:0
-                                                          relativeDuration:1/5
-                                                                animations:^{
-//                                      testLabel1.frame = CGRectMake(0, 200, 40, 30);
-                                                                    testLabel1.backgroundColor = [UIColor redColor];
-                                  }];
-                                 
-                                  [UIView addKeyframeWithRelativeStartTime:1/5
-                                                          relativeDuration:1/5
-                                                                animations:^{
-//                                      testLabel1.frame = CGRectMake(50, 200, 40, 30);
-                                                                    testLabel1.backgroundColor = [UIColor greenColor];
-                                  }];
-//                                  [UIView addKeyframeWithRelativeStartTime:2/5
-//                                                          relativeDuration:1/5
-//                                                                animations:^{
-////                                      testLabel1.frame = CGRectMake(50, 0, 40, 30);
-//                                                                    testLabel1.backgroundColor = [UIColor grayColor];
-//                                  }];
-//                                  [UIView addKeyframeWithRelativeStartTime:3/5
-//                                                          relativeDuration:1/5
-//                                                                animations:^{
-////                                      testLabel1.frame = CGRectMake(100, 0, 40, 30);
-//                                                                    testLabel1.backgroundColor = [UIColor yellowColor];
-//                                  }];
-//                                  [UIView addKeyframeWithRelativeStartTime:4/5
-//                                                          relativeDuration:1/5
-//                                                                animations:^{
-////                                      testLabel1.frame = CGRectMake(100, 200, 40, 30);
-//                                                                    testLabel1.backgroundColor = [UIColor darkGrayColor];
-//                                  }];
-                                  
-                                  
-
-                              } completion:^(BOOL finished) { NSLog(@"结束了");
-                              }];
-    */
-
-    /*
-    [UIView animateKeyframesWithDuration:5
-                                   delay:0.0
-                                 options:(UIViewKeyframeAnimationOptionAllowUserInteraction)
-                              animations:^{ // 参数1: RelativeStartTime: 相对的开始时间 参数2: relativeDuration:相对持续时间
-                                  [UIView addKeyframeWithRelativeStartTime:0.0
-                                                          relativeDuration:1/2
-                                                                animations:^{
-                                                                    //                                      testLabel1.frame = CGRectMake(0, 200, 40, 30);
-//                                                                    testLabel1.backgroundColor = [UIColor redColor];
-                                                                    testLabel1.center = CGPointMake(80, 10);
-//                                                            testLabel1.center.x += 80.0;
-//                                                            testLabel1.center.y -= 10.0;
-                                                                }];
-                                  
-                                  
-                                  [UIView addKeyframeWithRelativeStartTime:1/2
-                                                          relativeDuration:1/2
-                                                                animations:^{
-                                                                    //                                      testLabel1.frame = CGRectMake(50, 200, 40, 30);
-//                                                                    testLabel1.backgroundColor = [UIColor greenColor];
-                                                                    testLabel1.center = CGPointMake(80, 80);
-                                                                }];
-     
-     
-     
-                                  
-                            } completion:^(BOOL finished) {
-                                  NSLog(@"结束了");
-                              }];
-    
-    
-    //上面这个动画本来想实现画线的效果 但是没有实现
-    */
-    
-    /*
-    [UIView animateKeyframesWithDuration:2 delay:0 options:UIViewKeyframeAnimationOptionCalculationModeCubic animations:^{
-        
-        
-
-        
-    } completion:^(BOOL finished) {
-        
-    }];
-    */
-    
-    
-    
-    /*
-     * 效果:  testLabel 逐渐放大 然后逐渐透明 最终移除
-     */
-    /*
-    [UIView animateWithDuration:1.25 animations:^{
-        CGAffineTransform newTransform = CGAffineTransformMakeScale(1.8, 1.8);
-        [testLabel1 setTransform:newTransform];
-    }
-                     completion:^(BOOL finished){
-                         [UIView animateWithDuration:1.2 animations:^{
-                             [testLabel1 setAlpha:0];
-                             
-                         } completion:^(BOOL finished){
-                                 [testLabel1 removeFromSuperview];
-                         }];
-                     }];
-    */
 }
 - (void)stopLabel
 {
@@ -242,7 +101,114 @@
 //    testLabel1.frame = CGRectMake(320 - 40, 500, 80, 60);
     testLabel1.backgroundColor = [UIColor blueColor];
 }
-
+- (void)resetFrame:(UIButton *)bt{
+    if (![parentView.subviews containsObject:testLabel1]) {
+        [parentView addSubview:testLabel1];
+    }
+    testLabel1.alpha = 1.0;
+    testLabel1.frame = CGRectMake(0, 0, 40, 30);
+}
+- (void)beginAnimationClick{
+#pragma mark 基本用法2: UIViewAnimation 最简单的Block动画
+    /*
+    [UIView animateWithDuration:2 animations:^{
+        testLabel1.frame = CGRectMake(300 - 80, 200 - 60, 80, 60) ;
+    }];
+    */
+#pragma mark 基本用法3: UIViewAnimation Block动画嵌套
+    /*
+    [UIView animateWithDuration:2 animations:^{
+        testLabel1.frame = CGRectMake(300 - 80, 200 - 60, 80, 60) ;
+    } completion:^(BOOL finished) {
+        
+        [UIView animateWithDuration:2 animations:^{
+            testLabel1.backgroundColor = [UIColor redColor];
+        }];
+    }];
+    */
+#pragma mark 基本用法4: UIViewAnimation 实现动画重复
+    //UIViewAnimationOptionRepeat //单一则只是重复动画的过程
+    //UIViewAnimationOptionAutoreverse //单一 重复动画的反过程
+    //以上两者合并 则实现动画重复
+    /*
+    [UIView animateWithDuration:2 delay:0 options:UIViewAnimationOptionRepeat|UIViewAnimationOptionAutoreverse|UIViewAnimationOptionCurveEaseIn animations:^{
+        testLabel1.frame = CGRectMake(300 - 80, 200 - 60, 80, 60) ;
+    } completion:^(BOOL finished) {
+        
+    }];
+    */
+#pragma mark 基本用法5: UIViewAnimation 放大效果 transform的使用
+    
+     //动画 放大 后消失
+    /*
+    [UIView animateWithDuration:2 animations:^{
+        testLabel1.transform = CGAffineTransformMakeScale(1.5f, 1.5f);
+        testLabel1.alpha = 0.f;
+    } completion:^(BOOL finished) {
+        [testLabel1 removeFromSuperview];
+    }];
+     */
+    
+#pragma mark 基本用法4: UIViewAnimation 弹簧效果
+    
+    //有一个问题 transform效果 在重置后 再次启动时 失效
+    /*
+    [UIView animateWithDuration:2 delay:0 usingSpringWithDamping:0.1 initialSpringVelocity:15 options:UIViewAnimationOptionCurveEaseInOut animations:^{
+        testLabel1.center = CGPointMake(150 - 5, testLabel1.center.y);
+        
+    } completion:^(BOOL finished) {
+        
+        [UIView animateWithDuration:2 animations:^{
+            testLabel1.transform = CGAffineTransformMakeScale(3.5f, 3.5f);
+            testLabel1.alpha = 0.f;
+        } completion:^(BOOL finished) {
+//            [testLabel1 removeFromSuperview];
+        }];
+    }];
+    */
+     //小结:动画可以串联
+     //作业: 如何实现输入框在输入错误的情况下 抖动
+#pragma mark 基本用法4:UIViewAnimation 关键帧动画
+    
+    [UIView animateKeyframesWithDuration:5 delay:0 options:(UIViewKeyframeAnimationOptionCalculationModePaced) animations:^{
+        /* 参数1: RelativeStartTime: 相对的开始时间 
+         参数2: relativeDuration:相对持续时间 
+         options: UIViewKeyframeAnimationOptionRepeat|UIViewKeyframeAnimationOptionAutoreverse 和其他一样 两者同时使用 动画 正反过程 重复
+         UIViewKeyframeAnimationOptionLayoutSubviews 没啥特别的
+         
+         
+         */
+        [UIView addKeyframeWithRelativeStartTime:0.0 relativeDuration:0.3 animations:^{ testLabel1.center = CGPointMake(320/2, 0);
+            NSLog(@"关键帧11111");
+        }];
+        [UIView addKeyframeWithRelativeStartTime:0.3 relativeDuration:0.2 animations:^{ testLabel1.frame = CGRectMake(100, 50, 80, 60);
+            NSLog(@"关键帧22222");
+        }];
+        [UIView addKeyframeWithRelativeStartTime:0.5 relativeDuration:0.3 animations:^{ testLabel1.frame = CGRectMake(100, 300, 40, 30);
+            NSLog(@"关键帧3333");
+        }];
+        
+        [UIView addKeyframeWithRelativeStartTime:0.8 relativeDuration:0.2 animations:^{ testLabel1.frame = CGRectMake(300, 300, 40, 30);
+            NSLog(@"关键帧4444");
+        }];
+    } completion:^(BOOL finished) {
+        NSLog(@"结束了");
+    }];
+    //MARK:小结
+    /*
+     * 最后一个 StartTime + relativeDuration = 1, 这个必须的 ;
+     * 下一个的 StartTime = 前一个的 StartTime + relativeDuration
+     * frame的变化 的参考系是 其superView  即 parentView
+     */
+    
+    #pragma mark 基本用法4:
+    
+    
+    
+    #pragma mark 基本用法4:
+    #pragma mark 基本用法4:
+    
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
